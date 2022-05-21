8bc07950ae428332b3e69a6b0cdb3fcb9c2f97f3
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-05-21 22:06:58 +0300

    project init react router install firebase install tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+    require('tw-elements/dist/plugin')
+  ],
+}
